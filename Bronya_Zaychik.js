4c2ebaa7e5e6e54fba50343efc3499eacc9ff1a5
Deimissonvinicius <deimissonvinicius@gmail.com>
2026-07-16 17:15:12 -0300

    Add files via upload

---
+++ Bronya_Zaychik.js
@@ -0,0 +1,38 @@
+// let frase = '  Ola mundo  ';
+// console.log(frase.toLowerCase());
+// console.log(frase.toUpperCase());
+// console.log(frase.trim());
+// console.log(frase.trimStart());
+// console.log(frase.trimEnd());
+// console.log(frase.repeat(100));
+// console.log(frase.trim().repeat(100));
+
+// let frase2 = 'Olá Mundo';
+// console.log(frase2.length);
+// console.log(frase2[0]);
+// console.log(frase2[9]);
+// console.log(frase2.at(2));
+
+// let texto = 'Herrscher Of Reason';
+// let IncludeTexto = texto.includes('Reason');
+// console.log(IncludeTexto);
+// let a = texto.startsWith('Herrscher');
+// console.log(a);
+// let b = texto.endsWith('Reason');
+// console.log(b);
+
+// let frase3 = 'o gato viu o gato';
+// console.log(frase3.replace('gato', 'cachorro'));
+// console.log(frase3.replaceAll('gato', 'cachorro'));
+
+//split
+// let csv = 'Kiana;Mei;Bronya;Seele';
+// let nomes = csv.split(';');
+// console.log(nomes);//[ "kiana", "Mei", "Bronya", "Seele"]
+// let apenasDoisNomes = csv.split(';', 2);
+// console.log(apenasDoisNomes);//[ "Kiana", "Mei"]
+// console.log('Hello'.split(''));//[ "h", "e", "l", "l", "o"]
+
+//padStart | padEnd
+// console.log('50'.padStart(5, "0"));//00050
+// console.log('Olá mundo'.padEnd(10, ","));
